cadc40aa6fec3174ab5d6cbac15f72785e0f0eb0
Kayo Vinícius <kayo.vinicius034@gmail.com>
2023-10-25 14:35:47 -0300

    Números Pares e Impares 0-500
    Fizemos um laço de repetição (while) para mostrar os números pares 0-500 e assim, sucessivamente

---
+++ Exercício 25-10/parEimpar.php
@@ -0,0 +1,19 @@
+<?php
+
+#NUMEROS PARES
+$num = 0;
+while ($num <= 500) {
+    echo $num . " ";
+    $num += 2;
+}
+echo ("<br>");
+echo ("<br>");
+echo ("<br>");
+#NÚMERO IMPARES
+$num = 499; 
+while ($num >= 0) {
+    echo $num . " ";
+    $num -= 2; 
+}
+
+?>
